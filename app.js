bfd68aefab50aaa7c6eb2a1f57123e0bd66ad793
Toba Ojo <toba@Tobas-MacBook-Air.local>
2020-08-05 20:33:10 +0100

    making changes to the slide show

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 //
-// const carosel = document.querySelector('.carousel-img');
-// const image1 = document.querySelector('#carosel-image-1');
-// const image2 = document.querySelector('#carosel-image-2');
-// const image3 = document.querySelector('#carosel-image-3');
-// const image4 = document.querySelector('#carosel-image-4');
-// const image5 = document.querySelector('#carosel-image-5');
+const carosel = document.querySelector('.carousel-img');
+const image1 = document.querySelector('#carosel-image-1');
+const image2 = document.querySelector('#carosel-image-2');
+const image3 = document.querySelector('#carosel-image-3');
+const image4 = document.querySelector('#carosel-image-4');
+const image5 = document.querySelector('#carosel-image-5');
+const images = []
+
+images.push(image1,image2,image3,image4,image5);
+
+changeImage(images);
+
+function changeImage(arr){
+  arr.forEach(function(image){
+    if (image.classlist.contains('opaque')) {
+      setTimeout(function() {
+          image.classList.remove('opaque');
+      },3000)
+    }
+
+    });
+}
+
 
 //const images = []
 
@@ -92,45 +109,45 @@
 
 
 
-document.querySelector('#button-1').addEventListener('click', nextImage);
-document.querySelector('#button-2').addEventListener('click', nextImage);
-// const image3 = document.querySelector('#button-3').addEventListener('click', currentImage(3));
-// const image4 = document.querySelector('#button-4').addEventListener('click', currentImage(4));
-// const image5 = document.querySelector('#button-5').addEventListener('click', currentImage(5));
-
-var curImage = 0;
-var numImages = 5;
-
-function nextImage() {
-    var e;
-    // remove showMe class from current image
-    e = document.getElementById("carosel-images" + curImage);
-    removeClass(e, "opaque");
-
-    // compute next image
-    curImage++;
-    if (curImage > numImages - 1) {
-        curImage = 0;
-    }
-
-    // add showMe class to next image
-    e = document.getElementById("carosel-images" + curImage);
-    addClass(e, "opaque");
-}
-
-function addClass(elem, name) {
-    var c = elem.className;
-    if (c) c += " ";  // if not blank, add a space separator
-    c += name;
-    elem.className = c;
-}
-
-function removeClass(elem, name) {
-    var c = elem.className;
-    elem.className = c.replace(name, "").replace(/\s+/g, " ").replace(/^\s+|\s+$/g, "");  // remove name and extra blanks
-}
-
-
+// document.querySelector('#button-1').addEventListener('click', nextImage);
+// document.querySelector('#button-2').addEventListener('click', nextImage);
+// // const image3 = document.querySelector('#button-3').addEventListener('click', currentImage(3));
+// // const image4 = document.querySelector('#button-4').addEventListener('click', currentImage(4));
+// // const image5 = document.querySelector('#button-5').addEventListener('click', currentImage(5));
+//
+// var curImage = 0;
+// var numImages = 5;
+//
+// function nextImage() {
+//     var e;
+//     // remove showMe class from current image
+//     e = document.getElementById("carosel-images" + curImage);
+//     removeClass(e, "opaque");
+//
+//     // compute next image
+//     curImage++;
+//     if (curImage > numImages - 1) {
+//         curImage = 0;
+//     }
+//
+//     // add showMe class to next image
+//     e = document.getElementById("carosel-images" + curImage);
+//     addClass(e, "opaque");
+// }
+//
+// function addClass(elem, name) {
+//     var c = elem.className;
+//     if (c) c += " ";  // if not blank, add a space separator
+//     c += name;
+//     elem.className = c;
+// }
+//
+// function removeClass(elem, name) {
+//     var c = elem.className;
+//     elem.className = c.replace(name, "").replace(/\s+/g, " ").replace(/^\s+|\s+$/g, "");  // remove name and extra blanks
+// }
+//
+//
 
 
 // var s = 0;
